63d6bf16a9407dae890fe28f73ff6b67715e10fb
Vivek Sharma <32999354+viveksharma8130@users.noreply.github.com>
2022-05-11 20:55:19 +0530

    Final Update

--- src/Form/App.js
+++ src/Form/App.js
@@ -2,28 +2,34 @@ import { useEffect, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { Col, Container, Form, FormGroup, Row } from "reactstrap";
 import { Card, IconButton } from "@material-ui/core";
+import emailjs from "emailjs-com";
 
 //Icons
 import KeyboardArrowLeftIcon from "@material-ui/icons/KeyboardArrowLeft";
 import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
 import SendIcon from "@material-ui/icons/Send";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 
 // Form Api
 import formData from "./form.json";
 import "./index.css";
 
-const RenderCheckbox = ({ register, fields }) => {
+const RenderCheckbox = ({ register, fields, name }) => {
   return (
     <Col lg="12">
       <h3>{fields[0].name}</h3>
-      {fields[0].option.map((field) => (
+      {fields[0].option.map((field, idx) => (
         <div className="custom-control custom-checkbox mb-3" key={field.id}>
           <input
             type="checkbox"
             className="custom-control-input"
-            {...register(field?.name ?? field?.optionKey ?? field.question)}
+            value={field.question}
+            {...register(
+              `${fields[0].inputName}${field.optionKey ? "@" : ""}${
+                field.optionKey ?? ""
+              }`
+            )}
             id={"mycheckbox" + field.id}
-            value={field?.name ?? field?.optionKey ?? field.question}
           />
           <label
             className="custom-control-label"
@@ -37,7 +43,7 @@ const RenderCheckbox = ({ register, fields }) => {
   );
 };
 
-const RadioInput = ({ options, register, name }) => {
+const RadioInput = ({ options, register, name, inputName }) => {
   return (
     <Col lg="12">
       <h3>{name}</h3>
@@ -46,7 +52,7 @@ const RadioInput = ({ options, register, name }) => {
           <input
             className="custom-control-input"
             type="radio"
-            {...register(name)}
+            {...register(inputName)}
             value={opt.question}
             id={"myradio" + opt.id}
           />
@@ -69,7 +75,8 @@ const renderContactInput = (field, register) => {
       return (
         <RadioInput
           register={register}
-          name={field.question}
+          name={field.name}
+          inputName={field.inputName}
           options={field.options}
         />
       );
@@ -83,6 +90,7 @@ const renderContactInput = (field, register) => {
             placeholder="Type here..."
             rows="5"
           />
+          <br />
           <br />
           <br />
           <br />
@@ -121,10 +129,7 @@ const ContactForm = ({ register, fields }) => {
           key={field.id}
         >
           <div className="contact-input-container">
-            {field.question ===
-            "Have you worked with any agency in the past?" ? null : (
-              <label>{field.question}</label>
-            )}
+            <label>{field.question}</label>
             {renderContactInput(field, register)}
           </div>
         </Col>
@@ -171,21 +176,34 @@ const App = () => {
   // states
   const [currentPage, setCurrentPage] = useState(0);
   const [totalPages, setTotalPages] = useState(pages.current.length);
+  const [messageSend, setMessageSend] = useState(false);
 
   useEffect(() => {
     const subscription = watch((value, { name, type }) => {
-      if (!watchableFieldNames.includes(name)) return;
+      // console.log("form", value, name);
+      const optionKey = name.split("@")[1];
+      if (!watchableFieldNames.includes(optionKey)) return;
       // grab the value
       const isChecked = value[name];
 
+      console.log(name);
       if (isChecked) {
         // remove the contact form from last idx
-        const contactComp = [pages.current.pop(), pages.current.pop()];
-        formData[name].forEach(({ name, options }) => {
+        const contactComp = [
+          pages.current.pop(),
+          pages.current.pop(),
+        ].reverse();
+
+        formData[optionKey].forEach((item) => {
           pages.current.push({
             name,
             component: (
-              <RadioInput name={name} register={register} options={options} />
+              <RadioInput
+                name={item.name}
+                inputName={item.inputName}
+                register={register}
+                options={item.options}
+              />
             ),
           });
         });
@@ -197,15 +215,47 @@ const App = () => {
         // get the length from json
       }
       setTotalPages(pages.current.length);
-      // console.log(pages.current);
-      // console.log(name, type, value);
     });
     return () => subscription.unsubscribe();
   }, [watch, register]);
 
-  const submit = (values) => {
-    for (const [keys, value] of Object.entries(values)) {
-      console.log(keys, ":", value);
+  const submit = async (data) => {
+    for (const key in data) {
+      const val = data[key];
+      if (val === false || val.includes(false)) {
+        delete data[key];
+      }
+      if (key.includes("@")) {
+        const [name] = key.split("@");
+        if (data.hasOwnProperty(name) && Array.isArray(data[name])) {
+          // put the original value only if its true
+          val && data[name].push(val);
+        } else {
+          data[name] = [val];
+        }
+        delete data[key];
+      }
+    }
+
+    for (const key in data) {
+      const arrVals = data[key];
+      if (Array.isArray(arrVals)) {
+        data[key] = arrVals.join(", ");
+      }
+    }
+    console.log(data);
+    try {
+      const ser = "service_ai95l5u";
+      const temps = "template_rl6by88";
+      const users = "user_ySvYJMjB8dCxYhy6bksDq";
+      const res = await emailjs.send(ser, temps, data, users);
+      if (res.status === 200) {
+        setMessageSend(true);
+      } else {
+        setMessageSend(false);
+      }
+    } catch (e) {
+      console.log(e);
     }
   };
 
@@ -216,41 +266,61 @@ const App = () => {
           <Row>
             <Col className="col-lg-12">
               <Card className="myshadow">
-                <Form method="post" onSubmit={handleSubmit(submit)}>
+                {messageSend ? (
                   <div className="form_inner_wrap">
                     <div className="form">
-                      <Row>
-                        {pages.current[currentPage].component}
-                        <div className="action_btn">
-                          <IconButton
-                            onClick={() => setCurrentPage(currentPage - 1)}
-                            disabled={!currentPage}
-                          >
-                            <KeyboardArrowLeftIcon
-                              fontSize="large"
-                              className="icons"
-                            />
-                          </IconButton>
-                          {currentPage === totalPages - 1 ? (
-                            <IconButton type="submit">
-                              <SendIcon fontSize="large" className="icons" />
+                      <div className="successmsg">
+                        <div className="icon">
+                          <CheckCircleIcon fontSize="large" />
+                        </div>
+                        <div className="info">
+                          <h3>Thank you for contacting Waterfire!</h3>
+                          <p>
+                            We’re very excited to hear from you! Your message is
+                            in good hands. In the meantime, learn more about
+                            what makes us the best digital marketing company,
+                            and follow us on social media!
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  <Form method="post" onSubmit={handleSubmit(submit)}>
+                    <div className="form_inner_wrap">
+                      <div className="form">
+                        <Row>
+                          {pages.current[currentPage].component}
+                          <div className="action_btn">
+                            <IconButton
+                              onClick={() => setCurrentPage(currentPage - 1)}
+                              disabled={!currentPage}
+                            >
+                              <KeyboardArrowLeftIcon
+                                fontSize="large"
+                                className="icons"
+                              />
                             </IconButton>
-                          ) : null}
+                            {currentPage === totalPages - 1 ? (
+                              <IconButton type="submit">
+                                <SendIcon fontSize="large" className="icons" />
+                              </IconButton>
+                            ) : null}
 
-                          <IconButton
-                            onClick={() => setCurrentPage(currentPage + 1)}
-                            className={
-                              currentPage === totalPages - 1
-                                ? "d-none"
-                                : "d-block"
-                            }
-                          >
-                            <KeyboardArrowRightIcon
-                              fontSize="large"
-                              className="icons"
-                            />
-                          </IconButton>
-                          {/* <button
+                            <IconButton
+                              onClick={() => setCurrentPage(currentPage + 1)}
+                              className={
+                                currentPage === totalPages - 1
+                                  ? "d-none"
+                                  : "d-block"
+                              }
+                            >
+                              <KeyboardArrowRightIcon
+                                fontSize="large"
+                                className="icons"
+                              />
+                            </IconButton>
+                            {/* <button
                           // disable button when current page idx is 0
                           disabled={!currentPage}
                           onClick={() => setCurrentPage(currentPage - 1)}
@@ -265,11 +335,12 @@ const App = () => {
                         >
                           Next
                         </button> */}
-                        </div>
-                      </Row>
+                          </div>
+                        </Row>
+                      </div>
                     </div>
-                  </div>
-                </Form>
+                  </Form>
+                )}
               </Card>
             </Col>
           </Row>
